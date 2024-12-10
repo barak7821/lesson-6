5d41081c98f4eb93b68ba57c733ef7716ed23b4a
barak7821 <barak7821@gmail.com>
2024-12-10 16:31:48 +0200

    2

--- script.js
+++ script.js
@@ -18,3 +18,24 @@ checkId("1234567899")
 checkId("12345678t")
 
 // שאלה 2
+
+function primeNumber(number) {
+
+    if (number <= 1) {
+        return false
+    }
+
+    let squareroot = Math.sqrt(number)
+
+    for (let index = 2; index <= squareroot; index++) {
+        if (number % index === 0) {
+            return false
+        }
+    }
+    return true
+}
+
+console.log(primeNumber(5))
+console.log(primeNumber(12))
+console.log(primeNumber(1))
+console.log(primeNumber(11))
